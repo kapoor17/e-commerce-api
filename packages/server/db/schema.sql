19894360e0f1f270d49f3fe107a2647ab308bc1e
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-12 00:05:24 +0530

    Database: schema creation and other minor tasks

--- packages/server/db/schema.sql
+++ packages/server/db/schema.sql
@@ -18,21 +18,23 @@ ON Address (country);
 
 CREATE TABLE Cart (
     id uuid DEFAULT uuid_generate_v4(),
+    customer_id uuid,
     created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp,
-    customer_id uuid REFERENCES Customer(id) NOT NULL,
+    updated_at timestamp DEFAULT current_timestamp
 );
 
 CREATE TABLE Customer (
     id uuid DEFAULT uuid_generate_v4(),
     first_name varchar(20) NOT NULL,
     last_name varchar(20) NOT NULL,
+    gender varchar(10) NOT NULL,
     email varchar(255) UNIQUE NOT NULL,
     password varchar NOT NULL,
     address_id uuid REFERENCES Address(id),
+    cart_id uuid,
     created_at timestamp DEFAULT current_timestamp,
     PRIMARY KEY(id)
-)
+);
 
 ALTER TABLE Cart
 ADD FOREIGN KEY (customer_id) REFERENCES Customer(id);
@@ -46,21 +48,22 @@ ON Customer (first_name, last_name);
 CREATE INDEX customer_email_idx
 ON Customer (email);
 
-CREATE TABLE Order (
+CREATE TABLE "Order" (
     id uuid DEFAULT uuid_generate_v4(),
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp,
+    payment_method varchar(50) NOT NULL,
     status varchar(50) NOT NULL,
     total_amount money NOT NULL,
     customer_id uuid REFERENCES Customer(id) NOT NULL,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
     PRIMARY KEY(id)
-)
+);
 
 CREATE INDEX order_status_idx
-ON Order (status);
+ON "Order" (status);
 
 CREATE INDEX order_total_amount_idx
-ON Order (total_amount DESC);
+ON "Order" (total_amount DESC);
 
 CREATE TABLE Brand (
     id uuid DEFAULT uuid_generate_v4(),
@@ -68,14 +71,16 @@ CREATE TABLE Brand (
     description text,
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp,
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE Product (
     id uuid DEFAULT uuid_generate_v4(),
     name varchar(50) NOT NULL,
     description text,
     price money NOT NULL,
-    brand_id varchar(50) REFERENCES Brand(id) NOT NULL,
+    old_price money,
+    brand_id uuid REFERENCES Brand(id) NOT NULL,
     rating smallint NOT NULL,
     category varchar(50),
     stock_level integer NOT NULL,
@@ -88,8 +93,11 @@ CREATE TABLE Product (
 ALTER TABLE Product
 ADD CHECK (rating >= 0 AND rating =< 5);
 
-ALTER TABLE stock_level
+ALTER TABLE Product
 ADD CHECK (stock_level >= 0);
+
+CREATE INDEX product_price_id_idx
+ON Product (price_id);
 
 CREATE INDEX product_brand_id_idx
 ON Product (brand_id);
@@ -102,17 +110,22 @@ ON Product (category);
 
 CREATE TABLE OrderItem (
     id uuid DEFAULT uuid_generate_v4(),
-    order_id uuid REFERENCES Order(id) NOT NULL,
+    quantity smallint NOT NULL,
+    total_amount money NOT NULL,
+    order_id uuid REFERENCES "Order"(id) NOT NULL,
     product_id uuid REFERENCES Product(id) NOT NULL,
     PRIMARY KEY(id)
 );
+
+ALTER TABLE OrderItem
+ADD CHECK (quantity >= 0);
 
 CREATE INDEX orderitem_order_id_idx
 ON OrderItem (order_id);
 
 CREATE TABLE CartItem (
     id uuid DEFAULT uuid_generate_v4(),
-    quantity integer NOT NULL,
+    quantity smallint NOT NULL,
     cart_id uuid REFERENCES Cart(id) NOT NULL,
     product_id uuid REFERENCES Product(id) NOT NULL,
     created_at timestamp DEFAULT current_timestamp,
