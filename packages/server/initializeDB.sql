d8517be8cc27f3867ce31a1066f2d351b0fd0a7e
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-11 13:48:36 +0530

    Database: Schema refinemenet Added better constraints and created indexes

--- packages/server/initializeDB.sql
+++ packages/server/initializeDB.sql
@@ -2,22 +2,49 @@ CREATE TABLE Address (
     id uuid DEFAULT uuid_generate_v4(),
     street varchar(50) NOT NULL,
     city varchar(50) NOT NULL,
-    state varchar(50) NOT NULL,
-    country varchar(50) NOT NULL,
-    customer_id uuid NOT NULL,
+    state char(2) NOT NULL,
+    country char(2) NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE INDEX address_city_idx
+ON Address (city);
+
+CREATE INDEX address_state_idx
+ON Address (state);
+
+CREATE INDEX address_country_idx
+ON Address (country);
+
+CREATE TABLE Cart (
+    id uuid DEFAULT uuid_generate_v4(),
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
+    customer_id uuid REFERENCES Customer(id) NOT NULL,
 );
 
 CREATE TABLE Customer (
     id uuid DEFAULT uuid_generate_v4(),
     first_name varchar(20) NOT NULL,
     last_name varchar(20) NOT NULL,
-    email varchar(50) UNIQUE NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
     password varchar NOT NULL,
-    address_id uuid REFERENCES Address(id) NOT NULL,
+    address_id uuid REFERENCES Address(id),
     created_at timestamp DEFAULT current_timestamp,
     PRIMARY KEY(id)
 )
+
+ALTER TABLE Cart
+ADD FOREIGN KEY (customer_id) REFERENCES Customer(id);
+
+ALTER TABLE Customer
+ADD FOREIGN KEY (cart_id) REFERENCES Cart(id);
+
+CREATE INDEX customer_first_name_last_name_idx
+ON Customer (first_name, last_name);
+
+CREATE INDEX customer_email_idx
+ON Customer (email);
 
 CREATE TABLE Order (
     id uuid DEFAULT uuid_generate_v4(),
@@ -29,17 +56,49 @@ CREATE TABLE Order (
     PRIMARY KEY(id)
 )
 
-CREATE TABLE Product (
-    id uuid DEFAULT uuid_generate_v4,
-    name varchar(50) NOT NULL,
-    description varchar(100),
-    price money NOT NULL,
+CREATE INDEX order_status_idx
+ON Order (status);
+
+CREATE INDEX order_total_amount_idx
+ON Order (total_amount DESC);
+
+CREATE TABLE Brand (
+    id uuid DEFAULT uuid_generate_v4(),
+    name varchar(50) UNIQUE NOT NULL,
+    description text,
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp,
-    brand varchar(50) NOT NULL,
-    rating integer NOT NULL,
+)
+
+CREATE TABLE Product (
+    id uuid DEFAULT uuid_generate_v4(),
+    name varchar(50) NOT NULL,
+    description text,
+    price money NOT NULL,
+    brand_id varchar(50) REFERENCES Brand(id) NOT NULL,
+    rating smallint NOT NULL,
+    category varchar(50),
+    stock_level integer NOT NULL,
+    image_url varchar(255),
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
     PRIMARY KEY(id)
 );
+
+ALTER TABLE Product
+ADD CHECK (rating >= 0 AND rating =< 5);
+
+ALTER TABLE stock_level
+ADD CHECK (stock_level >= 0);
+
+CREATE INDEX product_brand_id_idx
+ON Product (brand_id);
+
+CREATE INDEX product_rating_idx
+ON Product (rating);
+
+CREATE INDEX product_category_idx
+ON Product (category);
 
 CREATE TABLE OrderItem (
     id uuid DEFAULT uuid_generate_v4(),
@@ -48,18 +107,20 @@ CREATE TABLE OrderItem (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE Cart (
-    id uuid DEFAULT uuid_generate_v4(),
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp,
-    customer_id uuid REFERENCES Customer(id) NOT NULL,
-);
+CREATE INDEX orderitem_order_id_idx
+ON OrderItem (order_id);
 
 CREATE TABLE CartItem (
     id uuid DEFAULT uuid_generate_v4(),
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp,
     quantity integer NOT NULL,
     cart_id uuid REFERENCES Cart(id) NOT NULL,
     product_id uuid REFERENCES Product(id) NOT NULL,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
 );
+
+ALTER TABLE CartItem
+ADD CHECK (quantity >= 1 AND quantity <=5);
+
+CREATE INDEX cartitem_cart_id_idx
+ON CartItem (cart_id);
