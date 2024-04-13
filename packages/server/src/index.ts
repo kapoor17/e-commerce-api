5929a07eef0fae736629d40124e9f3ac11f13898
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-04-13 13:34:24 +0530

    Deleted index file re added and schema file deleted

---
+++ packages/server/src/index.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import { connectDB } from './db/index.js';
+
+const app = express();
+
+app.set('trust proxy', 1)
+app.use(bodyParser.json())
+app.use(cors());
+app.use(helmet());
+
+const PORT = process.env.PORT;
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(PORT, () => 
+            console.log(`Server listening at PORT: ${PORT}`)
+        )
+    }catch(error){
+        console.error(`Error while connecting to the server: ${error}`);
+    }
+}
+startServer();
