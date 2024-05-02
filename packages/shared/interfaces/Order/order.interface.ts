a5e2c6ae5da763110c39c4d97f98e9d43edb52b3
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-05-02 15:59:46 +0530

    status zod schema updated to have literal values

--- packages/shared/interfaces/Order/order.interface.ts
+++ packages/shared/interfaces/Order/order.interface.ts
@@ -1,14 +1,38 @@
 import * as z from 'zod';
-import errorMessages from '../../messages/error.messages'
+import errorMessages from '../../messages/error.messages';
 
 export const Order = z.object({
-    id: z.string().uuid({message: errorMessages.invalid('UUID')}),
-    payment_method: z.literal(5,{message: errorMessages.min('Payment Method', 5)}),
-    status: z.literal(5, {message: errorMessages.min('Status', 5)}),
-    total_amount: z.number().min(0, {message: errorMessages.min('Total Amount', 0)}),
-    customer_id: z.string().uuid({message: errorMessages.invalid('UUID')}),
-    created_at: z.date({message: errorMessages.invalid('Date')}),
-    updated_at: z.date({message: errorMessages.invalid('Date')})
-})
+  id: z.string().uuid({ message: errorMessages.invalid('UUID') }),
+  status: z
+    .literal('created')
+    .or(
+      z
+        .literal('payed')
+        .or(
+          z
+            .literal('confirmed')
+            .or(
+              z
+                .literal('dispatched')
+                .or(
+                  z
+                    .literal('shipped')
+                    .or(
+                      z
+                        .literal('received')
+                        .or(z.literal('returned').or(z.literal('refunded')))
+                    )
+                )
+            )
+        )
+    ),
+  payment_method: z.literal(5, { message: errorMessages.min('Status', 5) }),
+  total_amount: z
+    .number()
+    .min(0, { message: errorMessages.min('Total Amount', 0) }),
+  customer_id: z.string().uuid({ message: errorMessages.invalid('UUID') }),
+  created_at: z.date({ message: errorMessages.invalid('Date') }),
+  updated_at: z.date({ message: errorMessages.invalid('Date') })
+});
 
-export type Order = z.infer<typeof Order>
+export type Order = z.infer<typeof Order>;
