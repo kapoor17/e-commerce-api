fc37b46d03ff9fe7de0cb2b441e268cde642ecc2
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-05-07 14:14:56 +0530

    base prettier file added

--- packages/shared/interfaces/Product/brand.interface.ts
+++ packages/shared/interfaces/Product/brand.interface.ts
@@ -1,12 +1,17 @@
 import * as z from 'zod';
-import errorMessages from '../../messages/error.messages'
+import errorMessages from '../../messages/error.messages';
 
 export const Brand = z.object({
-    id: z.string().uuid({message: errorMessages.invalid('UUID')}),
-    name: z.string().min(5, {message: errorMessages.min('Name', 5)}).max(50, {message: errorMessages.max('Name', 50)}),
-    description: z.string().min(20, {message: errorMessages.min('Description', 20)}),
-    created_at: z.date({message: errorMessages.invalid('Date')}),
-    updated_at: z.date({message: errorMessages.invalid('Date')})
-})
+  id: z.string().uuid({ message: errorMessages.invalid('UUID') }),
+  name: z
+    .string()
+    .min(5, { message: errorMessages.min('Name', 5) })
+    .max(50, { message: errorMessages.max('Name', 50) }),
+  description: z
+    .string()
+    .min(20, { message: errorMessages.min('Description', 20) }),
+  created_at: z.date({ message: errorMessages.invalid('Date') }),
+  updated_at: z.date({ message: errorMessages.invalid('Date') })
+});
 
-export type Brand = z.infer<typeof Brand>
+export type Brand = z.infer<typeof Brand>;
